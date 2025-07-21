a082a82c2cc0d316c9e5ad138eee39727eaa63b0
Freedaos Adeboye <freedaosadeboye293@gmail.com>
2025-07-21 12:04:46 +0100

    fibonacci function

--- scripts/main.js
+++ scripts/main.js
@@ -52,3 +52,14 @@ function isPrime(num) {
   }
   return true;
 }
+
+//function for fibonacci sequence
+function generateFibonacci(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib;
+}
